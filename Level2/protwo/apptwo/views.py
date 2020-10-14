0b6c77613b447ae5a835b817cac805ba01303051
samrat101 <samratrao101@gmail.com>
2020-10-15 05:21:28 +0530

    signup page exercise

--- Level2/protwo/apptwo/views.py
+++ Level2/protwo/apptwo/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
-from apptwo.models import user
+# from apptwo.models import user
 # Create your views here.
+from apptwo.forms import NewUserForm
 
 def index(request):
     return render(request,'apptwo/index.html')
 
 def users(request):
-    user_list=user.objects.order_by('first_name')
-    user_dict={'users':user_list}
-    return render(request,'apptwo/users.html',context=user_dict)
+    form = NewUserForm()
+
+    if request.method == "POST":
+        form = NewUserForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print("ERROR FORM IS INVALID")
+
+    return render(request,'apptwo/users.html',{'form':form})

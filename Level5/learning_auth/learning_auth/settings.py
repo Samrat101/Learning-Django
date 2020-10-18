76efec7ab9c2aab5d1f57563225aae61a5397c8f
samrat101 <samratrao101@gmail.com>
2020-10-18 23:23:17 +0530

    register user task done

--- Level5/learning_auth/learning_auth/settings.py
+++ Level5/learning_auth/learning_auth/settings.py
@@ -87,13 +87,12 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
-
-PASSWORD_HASHERS=[
-    'django.contrib.auth.hasers.Argon2PasswordHasher'
-    'django.contrib.auth.hasers.BCryptSHA256PasswordHasher'
-    'django.contrib.auth.hasers.PBKDF2PasswordHasher',
-    'django.contrib.auth.hasers.BCryptPasswordHasher'
-    'django.contrib.auth.hasers.PBKDF2SHA1PasswordHasher'
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 
 AUTH_PASSWORD_VALIDATORS = [
